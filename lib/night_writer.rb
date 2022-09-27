c0c80b743e5e0ccebd355d461a1b7f8eae87e490
Lacey Weaver <jlweave89@gmail.com>
2022-09-27 16:07:48 -0600

    Outlined methods

---
+++ lib/night_writer.rb
@@ -0,0 +1,22 @@
+# require './lib/dictionary'
+#
+# class NightRider
+
+# file = File.open(ARGV[0], "r")
+# incoming_text = file.read
+# count = incoming_text.length
+# # file.close
+# # require "pry"; binding.pry
+# writer = File.open(ARGV[1], "w")
+# writer.write(incoming_text)
+# puts "Created '#{ARGV[1]}' containing #{incoming_text} characters"
+# writer.close
+
+
+# message_file = File.open(ARGV[0], 'r')
+#
+# message = message_file.read
+# count = message.length
+# braille_file = File.open(ARGV[1], 'w')
+# File.write(braille_file, message)
+# puts "Created #{ARGV[1]} containing #{count} characters"
